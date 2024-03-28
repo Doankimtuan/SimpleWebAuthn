c8355f8cf8c7d5f3f64cfd4580956f9c76652cf4
Doankimtuan <40130020+Doankimtuan@users.noreply.github.com>
2024-03-28 15:15:22 +0700

    Update bufferToUTF8String.ts

--- packages/browser/src/helpers/bufferToUTF8String.ts
+++ packages/browser/src/helpers/bufferToUTF8String.ts
@@ -3,5 +3,11 @@
  * string.
  */
 export function bufferToUTF8String(value: ArrayBuffer): string {
-  return new TextDecoder('utf-8').decode(value);
+  const uint8Array = new Uint8Array(value);
+
+  // Use the TextDecoder API to decode the Uint8Array as UTF-8
+  const decoder = new TextDecoder("utf-8");
+  const text = decoder.decode(uint8Array);
+
+  return text;
 }
